d67372797b2b50c5a098e1e935b755cbdd7a051c
sergeytashkinov <tashkinov2@gmail.com>
2024-10-23 11:44:49 +0300

    corrections

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-from http.client import HTTPException
-from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
 app = FastAPI(docs_url=None)
 
-class Hotel:
-    name: str
-    country: str
-    city: str
-    rooms_total: int
-    min_room_price: float
-    max_room_price: float
 
-    def __init__(self, name, country, city, rooms_total, min_room_price, max_room_price):
-        self.name = name
-        self.country = country
-        self.city = city
-        self.rooms_total = rooms_total
-        self.min_room_price = min_room_price
-        self.max_room_price = max_room_price
 
-    def __str__(self):
-        return f'{self.name} - {self.city}'
-
-hotel_1 = Hotel("Звезда", "Россия", "Москва", 100, 2000, 10000)
-hotel_2 = Hotel("Хаятт", "Россия", "Москва", 200, 40000, 400000)
-hotel_3 = Hotel("Хаятт", "США", "Сан-Франциско", 500, 40000, 400000)
-hotel_4 = Hotel("Люкс", "Россия", "Сочи", 200, 40000, 400000)
-hotel_5 = Hotel("Хилтон", "Великобритания", "Лондон", 200, 40000, 400000)
-
-db = {1: hotel_1, 2: hotel_2, 3: hotel_3, 4: hotel_4, 5: hotel_5}
+hotels = [{"id": 1, "name": "Hyatt", "rooms": 200}, {"id": 2, "name": "Hilton", "rooms": 200}, {"id": 3, "name": "Lotte", "rooms": 200}, {"id": 4, "name": "Ritz", "rooms": 200}]
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
@@ -45,30 +20,44 @@ async def custom_swagger_ui_html():
     )
 
 @app.get("/hotels")
-def get_all_hotels():
-    global db
-    hotels = list(db.values())
-    return hotels
+def get_hotels(
+        id: int | None = Query(None, description="Идентификатор"),
+        name: str | None = Query(None, description="Название отеля"),
+):
+    return [hotel for hotel in hotels if hotel["id"] == id and hotel["name"] == name]
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "Ok"}
+
+@app.post("/hotels")
+def create_hotel(name: str = Body(embed=True)):
+    global hotels
+    hotels.append({"id": hotels[-1]["id"] + 1, "name": name})
+    return {"status": "Ok"}
 
 @app.put("/hotels/{hotel_id}")
-def change_hotel(hotel_id: int, hotel: dict):
-    global db
-    del db[hotel_id]
-    db[hotel_id] = Hotel(**hotel)
-    return {"status": "OK"}
+def edit_hotel(hotel_id: int, name: str = Body(), rooms: str = Body()):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["name"] = name
+    hotel["rooms"] = rooms
+    return {"status": "Ok"}
 
 @app.patch("/hotels/{hotel_id}")
-def change_param(hotel_id: int, param: str, value:  Union[str, int]):
-    global db
-    hotel = db.get(hotel_id, None).__dict__
-    if hotel is not None:
-        try:
-            hotel[param] = value
-            del db[hotel_id]
-            db[hotel_id] = Hotel(**hotel)
-            return {"status": "OK"}
-        except:
-            raise HTTPException
+def edit_hotel(hotel_id: int, name: str | None = Body(None), rooms: str | None= Body(None)):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if name:
+        hotel["name"] = name
+    if rooms:
+        hotel["rooms"] = rooms
+    return {"status": "Ok"}
+
+
 
 
 
